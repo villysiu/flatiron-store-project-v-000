805cd40e27ffbd15bbe17040a15ae18ec83f80da
Villy Siu <villysiu@gmail.com>
2017-12-29 06:09:04 +0000

    automatically backed up by learn

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
-    @items = @category.items
+    @items = @category.items.select {|item| item.inventory > 0}
+  #  @items = Item.select {|item| item.inventory > 0}
   end
 end
